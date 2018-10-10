60a61e6216a97bedfe419c38b8a5a581c78f66e5
Tom Beck <tom@tastylounge.com>
2018-10-10 12:14:29 -0700

    fix header and update index page layout

--- src/assets/_js/main.js
+++ src/assets/_js/main.js
@@ -7,3 +7,18 @@ import 'bootstrap';
 // import App from './App';
 // import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
 
+$("a[href*='#']:not([href='#']):not('.carousel-control-next'):not('.carousel-control-prev')").click(function(e) {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    e.preventDefault();
+    
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 80
+      }, 1000);
+      window.location.hash = this.hash;
+      return false;
+    }
+  }
+});
